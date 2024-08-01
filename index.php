0f42d5d277e35961b8f0b808182011c517d32fb0
Ram2118211 <155993518+Ram2118211@users.noreply.github.com>
2024-08-01 10:55:56 -0600

    index.php
    Docuemntación en inglés y español

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 
-// Requires the file that contains the definition of the VideoJsViewerPlugin class
+// ENG - Requires the file that contains the definition of the VideoJsViewerPlugin class
+// ESP - Requiere el archivo que contiene la definición de la clase VideoJsViewerPlugin
+
 require_once('VideoJsViewerPlugin.inc.php');
 
-// Creates a new instance of the VideoJsViewerPlugin and returns it
+// ENG - Creates a new instance of the VideoJsViewerPlugin and returns it
+// ESP - Crea una nueva instancia de VideoJsViewerPlugin y la retorna
+
 return new VideoJsViewerPlugin();
 
 ?>
