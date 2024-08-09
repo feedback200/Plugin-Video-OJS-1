e89bd99eb11744758b09d7520f75470299eebe1a
feedback200 <155993830+feedback200@users.noreply.github.com>
2024-08-08 22:09:02 -0600

    Update VideoJsViewerPlugin.inc.php

--- VideoJsViewerPlugin.inc.php
+++ VideoJsViewerPlugin.inc.php
@@ -1,13 +1,9 @@
 <?php
 
 // Imports the base class for PKP generic plugins
-// ENG - Imports the base class for PKP generic plugins
-// ESP - Importa la clase base para los plugins genéricos de PKP
 import('lib.pkp.classes.plugins.GenericPlugin');
 
 // Defines the VideoJsViewer plugin class that extends GenericPlugin
-// ENG - Defines the VideoJsViewer plugin class that extends GenericPlugin
-// ESP - Define la clase del plugin VideoJsViewer que extiende GenericPlugin
 class VideoJsViewerPlugin extends GenericPlugin {
 
     /**
@@ -20,28 +16,16 @@ class VideoJsViewerPlugin extends GenericPlugin {
      */
     function register($category, $path, $mainContextId = null) {
         // Calls the register method of the base class
-        // ENG - Calls the register method of the base class
-        // ESP - Llama al método register de la clase base
         if (parent::register($category, $path, $mainContextId)) {
             // Checks if the plugin is enabled
-            // ENG - Checks if the plugin is enabled
-            // ESP - Verifica si el plugin está habilitado
             if ($this->getEnabled($mainContextId)) {
                 // For OPS: registers the hook for preprint galley viewing
-                // ENG - For OPS: registers the hook for preprint galley viewing
-                // ESP - Para OPS: registra el hook para la visualización de la galería de preprints
                 HookRegistry::register('PreprintHandler::view::galley', [$this, 'submissionCallback'], HOOK_SEQUENCE_LAST);
                 // For OJS: registers the hook for article galley viewing
-                // ENG - For OJS: registers the hook for article galley viewing
-                // ESP - Para OJS: registra el hook para la visualización de la galería del artículo
                 HookRegistry::register('ArticleHandler::view::galley', [$this, 'submissionCallback'], HOOK_SEQUENCE_LAST);
                 // For OJS: registers the hook for issue galley viewing
-                // ENG - For OJS: registers the hook for issue galley viewing
-                // ESP - Para OJS: registra el hook para la visualización de la galería del número
                 HookRegistry::register('IssueHandler::view::galley', [$this, 'issueCallback'], HOOK_SEQUENCE_LAST);
                 // Requires common application locale components
-                // ENG - Requires common application locale components
-                // ESP - Requiere componentes de localización comunes de la aplicación
                 AppLocale::requireComponents(LOCALE_COMPONENT_APP_COMMON);
             }
             return true;
@@ -75,49 +59,54 @@ class VideoJsViewerPlugin extends GenericPlugin {
      * @return bool true if successfully processed, false otherwise
      */
     function submissionCallback($hookName, $args) {
-        // Gets the arguments from the hook call
-        // ENG - Gets the arguments from the hook call
-        // ESP - Obtiene los argumentos de la llamada al hook
+        // Obtener argumentos del hook
         $request = $args[0];
         $issue = $args[1];
         $galley = $args[2];
         $submission = $args[3];
         $submissionFile = $galley->getFile();
-        $author = $submission->getCurrentPublication()->getData('authors')[0];
     
-        // Checks if the file is an MP4 video
-        // ENG - Checks if the file is an MP4 video
-        // ESP - Verifica si el archivo es un video MP4
+        // Obtener lista de autores
+        $authors = $submission->getCurrentPublication()->getData('authors');
+        $authorDetails = [];
+        foreach ($authors as $author) {
+            $authorDetails[] = [
+                'name' => $author->getFullName(),
+                'orcid' => $author->getData('orcid'),
+                'affiliation' => $author->getLocalizedAffiliation()
+            ];
+        }
+        
+        //Obtain other necessary data
+        $keywords = $submission->getCurrentPublication()->getLocalizedData('keywords');
+        $doi = $submission->getStoredPubId('doi');
+    
+        // Check if the file is an MP4 video
         if ($submissionFile->getData('mimetype') === 'video/mp4') {
-            // Gets the template manager
-            // ENG - Gets the template manager
-            // ESP - Obtiene el gestor de plantillas
             $templateMgr = TemplateManager::getManager($request);
-            // Assigns variables to the template
-            // ENG - Assigns variables to the template
-            // ESP - Asigna variables a la plantilla
             $templateMgr->assign(array(
                 'displayTemplateResource' => $this->getTemplateResource('display.tpl'),
                 'pluginUrl' => $request->getBaseUrl() . '/' . $this->getPluginPath(),
                 'galleyFile' => $submissionFile,
                 'issue' => $issue,
                 'submission' => $submission,
-                'author' => $author,
+                'authors' => $authorDetails,
                 'galleyTitle' => $galley->getLabel(),
                 'videoUrl' => $request->url(null, 'article', 'download', [$submission->getBestId(), $galley->getBestGalleyId()]),
                 'datePublished' => $submission->getCurrentPublication()->getData('datePublished'),
                 'page_title' => $submission->getLocalizedTitle(),
+                'keywords' => $keywords,
+                'doi' => $doi,
             ));
-            // Displays the template
-            // ENG - Displays the template
-            // ESP - Muestra la plantilla
+            
+           // Show the template
             $templateMgr->display($this->getTemplateResource('display.tpl'));
             return true;
         }
     
         return false;
-    }
-    
+    }     
+
     /**
      * Callback for issue viewing
      * 
@@ -127,23 +116,15 @@ class VideoJsViewerPlugin extends GenericPlugin {
      */
     function issueCallback($hookName, $params) {
         // Gets the parameters from the hook call
-        // ENG - Gets the parameters from the hook call
-        // ESP - Obtiene los parámetros de la llamada al hook
         $request = $params[0];
         $issue = $params[1];
         $galley = $params[2];
 
         // Checks if the file is an MP4 video
-        // ENG - Checks if the file is an MP4 video
-        // ESP - Verifica si el archivo es un video MP4
         if ($galley->getFileType() == 'video/mp4') {
             // Gets the template manager
-            // ENG - Gets the template manager
-            // ESP - Obtiene el gestor de plantillas
             $templateMgr = TemplateManager::getManager($request);
             // Assigns variables to the template
-            // ENG - Assigns variables to the template
-            // ESP - Asigna variables a la plantilla
             $templateMgr->assign(array(
                 'displayTemplateResource' => $this->getTemplateResource('display.tpl'),
                 'pluginUrl' => $request->getBaseUrl() . '/' . $this->getPluginPath(),
@@ -154,8 +135,6 @@ class VideoJsViewerPlugin extends GenericPlugin {
                 'datePublished' => $issue->getData('datePublished'),
             ));
             // Displays the template
-            // ENG - Displays the template
-            // ESP - Muestra la plantilla
             $templateMgr->display($this->getTemplateResource('display.tpl'));
             return true;
         }
